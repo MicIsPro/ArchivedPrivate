dc9a56a062f7687cc6f74633ba4cbc9ab9512e0b
Devotion <92207489+MicIsPro@users.noreply.github.com>
2025-10-14 23:06:47 +0200

    Update ArchivedMain.lua

--- ArchivedMain.lua
+++ ArchivedMain.lua
@@ -48,7 +48,7 @@ local Tabs = {
 local teleportPositions = {
     {name = "Bar", pos = Vector3.new(441, -7, 438)},
     {name = "Syndicate Office", pos = Vector3.new(-658, -8, 971)},
-    {name = "Syntheza", pos = Vector3.new(130, 30, 851)},
+    {name = "Darius", pos = Vector3.new(130, 30, 851)},
     {name = "Subway", pos = Vector3.new(48, 0, 681)},
     {name = "Dumpster Guy", pos = Vector3.new(298, 64, -149)}
 }
@@ -901,4 +901,5 @@ if events then
         fallDamageRemote:Destroy()
     end
 end
+
 
